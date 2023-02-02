efc20849a27c4c5557c589620c09b09cc96f9643
Albert Tolokonnikov <tolokonnikov.albert@gmail.com>
2023-02-03 00:24:11 +0200

    add dbcontext

---
+++ WorldCitiesAPI/Data/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorldCitiesAPI.Data.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<City> Citys { get; set; }
+        public DbSet<Country> Countrys { get; set; }
+    }
+}
