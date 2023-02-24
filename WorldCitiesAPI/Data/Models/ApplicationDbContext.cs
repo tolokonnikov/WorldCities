4b6944c0db13c22ea366f050fceec7c82b6fef40
Albert Tolokonnikov <tolokonnikov.albert@gmail.com>
2023-02-24 02:16:43 +0200

    add initial migration

--- WorldCitiesAPI/Data/Models/ApplicationDbContext.cs
+++ WorldCitiesAPI/Data/Models/ApplicationDbContext.cs
@@ -14,6 +14,25 @@ namespace WorldCitiesAPI.Data.Models
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<City>().HasKey(x => x.Id);
+            modelBuilder.Entity<City>().Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>().Property(x => x.Lat).HasColumnType("decimal(7,4)");
+            modelBuilder.Entity<City>().Property(x => x.Lon).HasColumnType("decimal(7,4)");
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Country>().HasKey(x => x.Id);
+            modelBuilder.Entity<Country>().Property(x => x.Id).IsRequired();
+
+            modelBuilder.Entity<City>().HasOne(x => x.Country).WithMany(x => x.Cities).HasForeignKey(x => x.CountryId);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
         public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
     }
